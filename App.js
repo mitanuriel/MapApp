2d97f29f54ea8c67da3ca8f4fa5a0a399035e672
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-03-22 17:59:37 +0100

    Set region, added useState

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import { useState } from 'react';
 
 export default function App() {
+  const [region, setRegion] = useState ({
+    latitude: 55,
+    longitude:12,
+    latitudeDelta:20,
+    longitudeDelta:20
+  })
+
   return (
     <View style={styles.container}>
-       <MapView style={styles.map} />
+       <MapView 
+       style={styles.map}
+       region={region}
+       onRegionChangeComplete={setRegion}
+        />
        <StatusBar style="auto" />
+       
     </View>
   );
 }
