068eb7a614ed12cdf480aa724c042048f43a5c12
mitanuriel <145558996+mitanuriel@users.noreply.github.com>
2025-03-22 18:35:41 +0100

    Added Marker component

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import { useState } from 'react';
 
 export default function App() {
+  const [markers, setMarkers] = useState([])
+
   const [region, setRegion] = useState ({
     latitude: 55,
     longitude:12,
@@ -11,13 +13,37 @@ export default function App() {
     longitudeDelta:20
   })
 
+  function addMarker(data){
+    const {latitude, longitude} = data.nativeEvent.coordinate
+    const newMarker = {
+      coordinate: {latitude, longitude},
+      key: data.timeStamp.toString(),
+      title: "Great place"
+    }
+    setMarkers([...markers, newMarker])
+  }
+
+  function onMarkerPressed(text){
+    alert("you pressed " + text)
+  }
+
   return (
     <View style={styles.container}>
-       <MapView 
-       style={styles.map}
-       region={region}
-       onRegionChangeComplete={setRegion}
-        />
+       <MapView
+        style={styles.map}
+        region={region}
+        onRegionChangeComplete={setRegion}
+        onLongPress={addMarker}
+      >
+        {markers.map(marker => (
+          <Marker
+            coordinate={marker.coordinate}
+            key={marker.key}
+            title={marker.title}
+            onPress={() => onMarkerPressed(marker.title)}
+          />
+        ))}
+      </MapView>
        <StatusBar style="auto" />
        
     </View>
